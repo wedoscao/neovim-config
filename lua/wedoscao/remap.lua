39bf39bcbc88a4a652415c36a6628e7a814b6c94
Sang Cao <hoaisang0806@gmail.com>
2024-02-01 19:30:19 +0700

    change keymap for Ex

--- lua/wedoscao/remap.lua
+++ lua/wedoscao/remap.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -17,9 +17,6 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd("so")
