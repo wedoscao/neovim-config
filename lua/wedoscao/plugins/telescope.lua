c539f402b667d43c6e2dfc642e99747a54e0133f
Sang Cao <hoaisang0806@gmail.com>
2024-02-05 22:14:47 +0700

    add keymap for telescope diagnostics

--- lua/wedoscao/plugins/telescope.lua
+++ lua/wedoscao/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		vim.keymap.set("n", "<leader>fe", builtin.diagnostics, {})
 		require("telescope").load_extension("workspaces")
 	end,
 }
