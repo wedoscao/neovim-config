fbb1a43de5eebf6553b4a18777a76648aef6c899
Sang Cao <hoaisang0806@gmail.com>
2024-02-08 11:05:52 +0700

    add colorscheme ui

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,8 +1,28 @@
-function SetColorscheme(colorscheme) 
-    local colorscheme = colorscheme or vim.g.colorscheme
-    vim.cmd.colorscheme(colorscheme)
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+local function set_colorscheme(colorscheme)
+	colorscheme = colorscheme or vim.g.colorscheme or "default"
+	vim.cmd.colorscheme(colorscheme)
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-SetColorscheme()
+set_colorscheme()
+
+local function set_colorscheme_ui()
+	local ui = require("wedoscao.ui")
+	local coloschemes = vim.fn.getcompletion("", "color")
+	for i, v in ipairs(coloschemes) do
+		if v == "default" then
+			table.remove(coloschemes, i)
+			table.insert(coloschemes, 1, "default")
+			break
+		end
+	end
+	ui.show_menu(coloschemes, function(selection)
+		if selection == "default" then
+			selection = vim.g.colorscheme or "default"
+		end
+		set_colorscheme(selection)
+	end, { title = "Colorscheme" })
+end
+
+vim.keymap.set("n", "<leader>co", set_colorscheme_ui)
