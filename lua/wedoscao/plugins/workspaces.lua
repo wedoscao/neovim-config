4e9db4cbfb00617fcbdd8b77e710c87bc07c2550
Sang Cao <hoaisang0806@gmail.com>
2024-02-08 10:43:16 +0700

    auto open nvim workspace and separate ui fn

--- lua/wedoscao/plugins/workspaces.lua
+++ lua/wedoscao/plugins/workspaces.lua
@@ -7,82 +7,7 @@ local function get_workspace_names()
 	return workspace_names
 end
 
-local function show_input(callback, opts)
-	if not opts then
-		opts = {}
-	end
-	local popup = require("plenary.popup")
-	local height = 1
-	local width = 30
-	local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
-
-	local win_id = popup.create("", {
-		title = opts.title and opts.title or "Input",
-		highlight = "InputWindow",
-		line = math.floor(((vim.o.lines - height) / 2) - 1),
-		col = math.floor((vim.o.columns - width) / 2),
-		minwidth = width,
-		minheight = height,
-		maxheight = height,
-		borderchars = borderchars,
-	})
-
-	local bufnr = vim.api.nvim_win_get_buf(win_id)
-	vim.keymap.set("n", "<Esc>", function()
-		vim.api.nvim_win_close(win_id, true)
-	end, { silent = false, buffer = bufnr })
-
-	vim.keymap.set("i", "<Enter>", function() end)
-
-	vim.keymap.set("n", "<Enter>", function()
-		local lines = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
-		local input = table.concat(lines, "\n")
-		callback(input)
-		vim.api.nvim_win_close(win_id, true)
-	end, { silent = false, buffer = bufnr })
-end
-
-local function show_menu(what, callback, opts)
-	if not opts then
-		opts = {}
-	end
-	local popup = require("plenary.popup")
-	local width = 30
-	local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
-	local height = 1
-	if #what > 20 then
-		height = 20
-	elseif #what > 1 then
-		height = #what
-	end
-
-	local win_id = popup.create(what, {
-		title = opts.title and opts.title or "Menu",
-		highlight = "MenuWindow",
-		line = math.floor(((vim.o.lines - height) / 2) - 1),
-		col = math.floor((vim.o.columns - width) / 2),
-		minwidth = width,
-		minheight = 1,
-		maxheight = height,
-		borderchars = borderchars,
-		callback = function(_, selection)
-			callback(selection)
-		end,
-	})
-	vim.cmd("set noma")
-
-	local bufnr = vim.api.nvim_win_get_buf(win_id)
-	vim.api.nvim_create_autocmd("WinClosed", {
-		buffer = bufnr,
-		callback = function()
-			vim.cmd("set ma")
-		end,
-	})
-
-	vim.keymap.set("n", "<Esc>", function()
-		vim.api.nvim_win_close(win_id, true)
-	end, { silent = false, buffer = bufnr })
-end
+local ui = require("wedoscao.ui")
 
 return {
 	"natecraddock/workspaces.nvim",
@@ -92,7 +17,7 @@ return {
 		vim.keymap.set("n", "<leader>wo", function()
 			workspaces.sync_dirs()
 			local workspace_names = get_workspace_names()
-			show_menu(workspace_names, function(selection)
+			ui.show_menu(workspace_names, function(selection)
 				workspaces.open(selection)
 			end, { title = "Open Workspace" })
 		end)
@@ -103,20 +28,20 @@ return {
 			for _, item in ipairs(workspaces.get()) do
 				table.insert(workspace_names, item.name)
 			end
-			show_menu(workspace_names, function(name)
+			ui.show_menu(workspace_names, function(name)
 				workspaces.remove(name)
 			end, { title = "Remove Workspace" })
 		end)
 
 		vim.keymap.set("n", "<leader>wad", function()
-			show_input(function(path)
+			ui.show_input(function(path)
 				local expanded_path = vim.fn.expand(path)
 				workspaces.add_dir(expanded_path)
 			end, { title = "Add Dir" })
 		end)
 
 		vim.keymap.set("n", "<leader>wrd", function()
-			show_input(function(path)
+			ui.show_input(function(path)
 				local expanded_path = vim.fn.expand(path)
 				workspaces.remove_dir(expanded_path)
 			end, { title = "Remove Dir" })
@@ -131,5 +56,6 @@ return {
 		if not has_nvim then
 			workspaces.add(vim.fn.expand("~/.config/nvim"), "nvim")
 		end
+		workspaces.open("nvim")
 	end,
 }
---
+++ lua/wedoscao/ui.lua
@@ -0,0 +1,81 @@
+local function show_input(callback, opts)
+	if not opts then
+		opts = {}
+	end
+	local popup = require("plenary.popup")
+	local height = 1
+	local width = 30
+	local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+
+	local win_id = popup.create("", {
+		title = opts.title and opts.title or "Input",
+		highlight = "InputWindow",
+		line = math.floor(((vim.o.lines - height) / 2) - 1),
+		col = math.floor((vim.o.columns - width) / 2),
+		minwidth = width,
+		minheight = height,
+		maxheight = height,
+		borderchars = borderchars,
+	})
+
+	local bufnr = vim.api.nvim_win_get_buf(win_id)
+	vim.keymap.set("n", "<Esc>", function()
+		vim.api.nvim_win_close(win_id, true)
+	end, { silent = false, buffer = bufnr })
+
+	vim.keymap.set("i", "<Enter>", function() end)
+
+	vim.keymap.set("n", "<Enter>", function()
+		local lines = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
+		local input = table.concat(lines, "\n")
+		callback(input)
+		vim.api.nvim_win_close(win_id, true)
+	end, { silent = false, buffer = bufnr })
+end
+
+local function show_menu(what, callback, opts)
+	if not opts then
+		opts = {}
+	end
+	local popup = require("plenary.popup")
+	local width = 30
+	local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+	local height = 1
+	if #what > 20 then
+		height = 20
+	elseif #what > 1 then
+		height = #what
+	end
+
+	local win_id = popup.create(what, {
+		title = opts.title and opts.title or "Menu",
+		highlight = "MenuWindow",
+		line = math.floor(((vim.o.lines - height) / 2) - 1),
+		col = math.floor((vim.o.columns - width) / 2),
+		minwidth = width,
+		minheight = 1,
+		maxheight = height,
+		borderchars = borderchars,
+		callback = function(_, selection)
+			callback(selection)
+		end,
+	})
+	vim.cmd("set noma")
+
+	local bufnr = vim.api.nvim_win_get_buf(win_id)
+	vim.api.nvim_create_autocmd("WinClosed", {
+		buffer = bufnr,
+		callback = function()
+			vim.cmd("set ma")
+		end,
+	})
+
+	vim.keymap.set("n", "<Esc>", function()
+		vim.api.nvim_win_close(win_id, true)
+	end, { silent = false, buffer = bufnr })
+end
+
+return {
+	show_input,
+	show_menu,
+}
