ec8aeb1af0d039019cc681e23ca3a6ac586f8fd7
Sang Cao <hoaisang0806@gmail.com>
2024-03-18 21:29:48 +0700

    telescope ignore image extensions

--- lua/wedoscao/plugins/telescope.lua
+++ lua/wedoscao/plugins/telescope.lua
@@ -3,6 +3,9 @@ return {
 	tag = "0.1.5",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-web-devicons" },
 	config = function()
+		require("telescope").setup({
+			defaults = { file_ignore_patterns = { "%.png", "%.jpg", "%.jpeg", "%.gif", "%.webp" } },
+		})
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
